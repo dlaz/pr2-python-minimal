05ff265530dacc59a5fd73946fc626cc1c037019
Dan Lazewatsky <dan@lazewatsky.com>
2015-03-24 11:21:12 -0500

    tweaks

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup_args = generate_distutils_setup(
     packages=['pr2_python'],
     package_dir={'': 'src'},
     requires=[], # TODO
+    scripts=['scripts/move_to_look.py', 'scripts/print_base_pose.py', 'scripts/print_joint_states.py', 'scripts/print_transform.py'],
 )
 
 setup(**setup_args)
